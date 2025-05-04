bc98425b419c6288fc6c71f7fb61991bc0aa1e5e
Felix Raithel <felix.m.raithel@outlook.de>
2025-05-04 18:14:17 +0200

    Added Structure

--- src/main/java/de/fixclient/gem_fabric/Main.java
+++ src/main/java/de/fixclient/gem_fabric/Main.java
@@ -5,8 +5,19 @@ import de.fixclient.gem_fabric.item.ItemManager;
 import de.fixclient.gem_fabric.item.ItemNames;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.StructureTemplate;
+import net.minecraft.structure.StructureTemplateManager;
 import net.minecraft.text.Text;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.ServerWorldAccess;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,5 +64,28 @@ public class Main implements ModInitializer {
         }));
 
         Gem_Command.register();
+
+        ServerWorldEvents.LOAD.register(((server, world) -> {
+            ServerWorld serverWorld = (ServerWorld) world;
+            StructureTemplateManager manager = serverWorld.getStructureTemplateManager();
+            Identifier templateId = Identifier.of(MOD_ID, "altar");
+            StructureTemplate template = manager.getTemplate(templateId).orElseThrow(() -> new RuntimeException("Struktur konnte nicht gefunden werden"));
+            Vec3i size = template.getSize();
+            BlockPos pivot = new BlockPos(size.getX() / 2, 0, size.getZ());
+            StructurePlacementData placementData = new StructurePlacementData()
+                    .setRotation(BlockRotation.NONE)
+                    .setMirror(BlockMirror.NONE)
+                    .setIgnoreEntities(false);
+            BlockPos spawn = serverWorld.getSpawnPos();
+            template.place(
+                    (ServerWorldAccess) serverWorld,
+                    spawn,
+                    pivot,
+                    placementData,
+                    server.getOverworld().getRandom(),
+                    2
+            );
+            serverWorld.setSpawnPos(new BlockPos(spawn.getX(), spawn.getY() + 4, spawn.getZ()), serverWorld.getSpawnAngle());
+        }));
     }
 }
